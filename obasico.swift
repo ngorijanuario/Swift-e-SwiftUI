eea92c806f13145ed2603694dfc90dc9925f03ca
Ngori Januário <68328526+ngorijanuario@users.noreply.github.com>
2023-01-02 11:34:26 +0100

    Dia 1

---
+++ obasico.swift
@@ -0,0 +1,58 @@
+
+/*
+ 
+ 
+ 
+ import Foundation
+
+ //Dia 1 - O Basico
+
+ /* ------------------------------------------------------------
+  Constantes e variaveis
+  Declarando constantes e variaveis
+  */
+ let nome = "Ngori" //Declaração de uma constante
+ var idade = 23 //Declaração de uma variavel
+
+ //Declaração de variaveis em uma unica linha
+ var x = 0.0, y = 0.0, z = 0.0
+
+ /* ------------------------------------------------------------
+  Anotação de tipos
+  */
+ var curso : String
+
+ var tamnho, peso, percentagem : Double // Anotação em uma linha
+
+ /* ------------------------------------------------------------
+  Nomeando constantes e variaveis
+  */
+ var continenete = "Africa"
+ continenete = "America"
+ /*
+  Imprimindo variaveis
+  */
+ print("Nome: \(nome)") //impressão com interpolação de String
+ print("Cordenadas:\nX = \(x) Y = \(y) e Z = \(z)")
+ print(continenete)
+
+ /* ------------------------------------------------------------
+  Converter int para double
+  */
+ var idadeConvertida = Double(idade)
+ var idadeDesconvertida = Int(idadeConvertida)
+ print(idadeConvertida, idadeDesconvertida)
+
+ /* ------------------------------------------------------------
+  Booleano
+  */
+ var opcaoVerdadeiro = true
+ var opcaoFalsa = false
+
+ if opcaoVerdadeiro{
+     print(opcaoVerdadeiro)
+ }
+
+ 
+ 
+ */
